7a6ff20595edf6514129b2de20b3da07a97833ea
Varun2616 <varunjha2616@gmail.com>
2026-08-26 03:04:28 +0530

    build: setup socket.io client and frontend dependencies

---
+++ src/socket.js
@@ -0,0 +1,17 @@
+import { io } from 'socket.io-client';
+
+const BACKEND_URL = 'http://localhost:5000';
+
+export const socket = io(BACKEND_URL, {
+  autoConnect: true,
+  reconnection: true
+});
+
+// Optionally log connection status
+socket.on('connect', () => {
+  console.log('Connected to WebSocket server:', socket.id);
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from WebSocket server');
+});
